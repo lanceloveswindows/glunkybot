6097d037e29c6cb7a9aa520ed8c8a02c9945aaa0
lanceloveswindows <raphael.atticus@gmail.com>
2025-11-15 18:56:30 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 # Choose a command prefix (e.g. '!')
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
